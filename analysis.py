6e955ed1ceaecc6a08673c2bc1826882db9c198a
Daria Bonds <bondsdaria@gmail.com>
2021-07-07 14:45:25 -0700

    Adding real script.

--- analysis.py
+++ analysis.py
@@ -1,2 +1,6 @@
-print('Hello world')
-print("testing")
+import yt
+
+ds = yt.load('../m12i_res56000_md/snapshot_600.hdf5')
+_, c = ds.find_max(('gas', 'density'))
+p = yt.ProjectionPlot(ds, 'x', ('gas', 'density'), center = c)
+p.save()
