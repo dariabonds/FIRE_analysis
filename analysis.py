9076cf1c46c1671bbfa50ce1f8018b2e697b712c
Daria Bonds <bondsdaria@gmail.com>
2021-07-07 15:09:48 -0700

    Zoomed to 100 kpc.

--- analysis.py
+++ analysis.py
@@ -2,5 +2,5 @@ import yt
 
 ds = yt.load('../m12i_res56000_md/snapshot_600.hdf5')
 _, c = ds.find_max(('gas', 'density'))
-p = yt.ProjectionPlot(ds, 'x', ('gas', 'density'), center = c)
+p = yt.ProjectionPlot(ds, 'x', ('gas', 'density'), center=c, width=(100, 'kpc'))
 p.save()
