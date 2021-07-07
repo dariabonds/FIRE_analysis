1cd08acaef719522b9df37cfc900ea8e57843374
Daria Bonds <bondsdaria@gmail.com>
2021-07-07 15:19:16 -0700

    Projected face on.

--- analysis.py
+++ analysis.py
@@ -2,5 +2,10 @@ import yt
 
 ds = yt.load('../m12i_res56000_md/snapshot_600.hdf5')
 _, c = ds.find_max(('gas', 'density'))
-p = yt.ProjectionPlot(ds, 'x', ('gas', 'density'), center=c, width=(100, 'kpc'))
+#p = yt.ProjectionPlot(ds, 'x', ('gas', 'density'), center=c, width=(100, 'kpc'))
+#p.save()
+
+sp = ds.sphere(c, (25.0, 'kpc'))
+L = sp.quantities.angular_momentum_vector()
+p = yt.OffAxisProjectionPlot(ds, L, ('gas', 'density'), center=c, width=(100, 'kpc'))
 p.save()
