08a1684d8bf2ed636aff995740645053b8af8f61
Daria Bonds <bondsdaria@gmail.com>
2021-07-07 15:44:45 -0700

    Projected edge on.

--- analysis.py
+++ analysis.py
@@ -1,4 +1,5 @@
 import yt
+from yt.utilities.math_utils import ortho_find
 
 ds = yt.load('../m12i_res56000_md/snapshot_600.hdf5')
 _, c = ds.find_max(('gas', 'density'))
@@ -7,5 +8,6 @@ _, c = ds.find_max(('gas', 'density'))
 
 sp = ds.sphere(c, (25.0, 'kpc'))
 L = sp.quantities.angular_momentum_vector()
-p = yt.OffAxisProjectionPlot(ds, L, ('gas', 'density'), center=c, width=(100, 'kpc'))
+L, edge1, edge2 = ortho_find(L)
+p = yt.OffAxisProjectionPlot(ds, edge2, ('gas', 'density'), center=c, width=(100, 'kpc'), north_vector=L)
 p.save()
