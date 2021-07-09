7e02bfd46dfa0c2f99c99dadc4710383a29a879b
Daria Bonds <bondsdaria@gmail.com>
2021-07-09 15:25:01 -0700

    Update to analysis script.

--- analysis.py
+++ analysis.py
@@ -2,14 +2,19 @@ import yt
 from yt.utilities.math_utils import ortho_find
 
 ##load in dataset
-#ds = yt.load('../m12i_res56000_md/snapshot_600.hdf5')
-ds = yt.load('../m12i_res7100_md/snapdir_600/snapshot_600.0.hdf5')
+ds = yt.load('../m12i_res56000_md/snapshot_600.hdf5')
+#ds = yt.load('../m12i_res7100_md/snapdir_600/snapshot_600.0.hdf5')
+
 ##find galaxy center
 _, c = ds.find_max(('gas', 'density'))
+#particular coord (x,y,z)
+#c = [29338.09863660, 30980.12414340, 32479.90455557]
+
 ##plot galaxy face on
 #p = yt.ProjectionPlot(ds, 'x', ('gas', 'density'), center=c, width=(100, 'kpc'))
-#p = yt.ProjectionPlot(ds, 'x', ('gas', 'density'), center=c, width=(50, 'kpc'))
+#p = yt.ProjectionPlot(ds, 'x', ('gas', 'density'), center=c, width=(10, 'Mpc'))
 #p.save()
+#import sys; sys.exit()
 
 ##create sphere
 #sp = ds.sphere(c, (25.0, 'kpc'))
@@ -18,10 +23,12 @@ sp = ds.sphere(c, (10.0, 'kpc'))
 L = sp.quantities.angular_momentum_vector()
 ##find orthogonal vectors
 L, edge1, edge2 = ortho_find(L)
+
 ##plot galaxy edge on
 #p = yt.OffAxisProjectionPlot(ds, edge2, ('gas', 'density'), center=c, width=(100, 'kpc'), north_vector=L)
 #p = yt.OffAxisProjectionPlot(ds, edge2, ('gas', 'density'), center=c, width=(50, 'kpc'), north_vector=L)
 #p.save()
+#import sys; sys.exit()
 
 ##create a phase plot
 #sp2 = ds.sphere(c, (250.0, 'kpc'))
@@ -33,10 +40,12 @@ sp2 = ds.sphere(c, (150.0, 'kpc'))
 ##set sphere center and angular momentum
 sp2.set_field_parameter('center', c)
 sp2.set_field_parameter('normal', L)
+
 ##create particle phase plot
-#p = yt.PhasePlot(sp2, ('PartType0', 'particle_position_spherical_radius'), ('PartType0', 'particle_position_spherical_phi'), ('gas', 'mass'), weight_field=None)
-#p.set_unit(('gas', 'mass'), 'Msun')
-#p.set_unit(('PartType0', 'particle_position_spherical_radius'), 'kpc')
+p = yt.PhasePlot(sp2, ('PartType0', 'particle_position_spherical_radius'), ('PartType0', 'particle_position_spherical_phi'), ('gas', 'mass'), weight_field=None)
+p.set_unit(('gas', 'mass'), 'Msun')
+p.set_unit(('PartType0', 'particle_position_spherical_radius'), 'kpc')
 # p.set_unit(('PartType0', 'particle_position_spherical_phi'), 'deg')
 # p.set_log(('PartType0', 'particle_position_spherical_phi'), False)
-#p.save()
+
+p.save()
