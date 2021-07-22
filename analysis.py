02d5d533f0f8895d4e360944eacf933e5adfb681
Daria Bonds <bondsdaria@gmail.com>
2021-07-22 11:24:06 -0700

    Update to working with larger dataset and angle bin names.

--- analysis.py
+++ analysis.py
@@ -6,47 +6,47 @@ from yt.utilities.math_utils import ortho_find
 from math import pi
 
 ##set particle filters #consider defining deg in function
-def angle_low(pfilter, data):
-    #filter = data[(pfilter.filtered_type, 'particle_position_spherical_theta')] > 0 and
+def angle_I(pfilter, data):
+    #filter = data[(pfilter.filtered_type, 'phi')] > 0 &
     filter = data[(pfilter.filtered_type, 'phi')] < 15
     return filter
 
-yt.add_particle_filter('angle_low', function=angle_low, filtered_type='PartType0', requires=['phi'])
+yt.add_particle_filter('angle_I', function=angle_I, filtered_type='PartType0', requires=['phi'])
 
-def angle_low_mid(pfilter, data):
+def angle_II(pfilter, data):
     filter = (data[(pfilter.filtered_type, 'phi')] > 15) \
             & (data[(pfilter.filtered_type, 'phi')] < 30)
     return filter
 
-yt.add_particle_filter('angle_low_mid', function=angle_low_mid, filtered_type='PartType0', requires=['phi'])
+yt.add_particle_filter('angle_II', function=angle_II, filtered_type='PartType0', requires=['phi'])
 
-def angle_mid_one(pfilter, data):
+def angle_III(pfilter, data):
     filter = (data[(pfilter.filtered_type, 'phi')] > 30) \
             & (data[(pfilter.filtered_type, 'phi')] < 45)
     return filter
 
-yt.add_particle_filter('angle_mid_one', function=angle_mid_one, filtered_type='PartType0', requires=['phi'])
+yt.add_particle_filter('angle_III', function=angle_III, filtered_type='PartType0', requires=['phi'])
 
-def angle_mid_two(pfilter, data):
+def angle_IV(pfilter, data):
     filter = (data[(pfilter.filtered_type, 'phi')] > 45) \
             & (data[(pfilter.filtered_type, 'phi')] < 60)
     return filter
 
-yt.add_particle_filter('angle_mid_two', function=angle_mid_two, filtered_type='PartType0', requires=['phi'])
+yt.add_particle_filter('angle_IV', function=angle_IV, filtered_type='PartType0', requires=['phi'])
 
-def angle_mid_high(pfilter, data):
+def angle_V(pfilter, data):
     filter = (data[(pfilter.filtered_type, 'phi')] > 60) \
             & (data[(pfilter.filtered_type, 'phi')] < 75)
     return filter
 
-yt.add_particle_filter('angle_mid_high', function=angle_mid_high, filtered_type='PartType0', requires=['phi'])
+yt.add_particle_filter('angle_V', function=angle_V, filtered_type='PartType0', requires=['phi'])
 
-def angle_high(pfilter, data):
+def angle_VI(pfilter, data):
     filter = (data[(pfilter.filtered_type, 'phi')] >= 75) \
             & (data[(pfilter.filtered_type, 'phi')] <= 90)
     return filter
 
-yt.add_particle_filter('angle_high', function=angle_high, filtered_type='PartType0', requires=['phi'])
+yt.add_particle_filter('angle_VI', function=angle_VI, filtered_type='PartType0', requires=['phi'])
 
 ##define angle phi
 ##phi: radians to degrees
@@ -60,26 +60,25 @@ yt.add_field(name=('PartType0', 'phi'), function=_phi, sampling_type='particle',
 ds = yt.load('../m12i_res56000_md/snapshot_600.hdf5')
 #ds = yt.load('../m12i_res7100_md/snapdir_600/snapshot_600.0.hdf5')
 
-##add filters to data
-ds.add_particle_filter('angle_low')
-ds.add_particle_filter('angle_low_mid')
-ds.add_particle_filter('angle_mid_one')
-ds.add_particle_filter('angle_mid_two')
-ds.add_particle_filter('angle_mid_high')
-ds.add_particle_filter('angle_high')
-
 ##find galaxy center
 _, c = ds.find_max(('gas', 'density'))
 ##specific coord (x,y,z)
-#c = [29338.09863660, 30980.12414340, 32479.90455557]
+#c = ds.arr([29338.09863660, 30980.12414340, 32479.90455557], "code_length")
+
+##add filters to data
+ds.add_particle_filter('angle_I')
+ds.add_particle_filter('angle_II')
+ds.add_particle_filter('angle_III')
+ds.add_particle_filter('angle_IV')
+ds.add_particle_filter('angle_V')
+ds.add_particle_filter('angle_VI')
 
 ##plot galaxy face on
 #p = yt.ProjectionPlot(ds, 'x', ('gas', 'density'), center=c, width=(100, 'kpc'))
-#p = yt.ProjectionPlot(ds, 'x', ('gas', 'density'), center=c, width=(10, 'Mpc'))
+#p = yt.ProjectionPlot(ds, 'x', ('gas', 'density'), center=c, width=(10, 'kpc'))
 #p.save()
 
 ##create sphere
-#sp = ds.sphere(c, (20.0, 'kpc'))
 sp1 = ds.sphere(c, (10.0, 'kpc'))
 ##find max angular momentum
 L = sp1.quantities.angular_momentum_vector()
@@ -88,12 +87,11 @@ L, edge1, edge2 = ortho_find(L)
 
 ##plot galaxy edge on
 #p = yt.OffAxisProjectionPlot(ds, edge2, ('gas', 'density'), center=c, width=(100, 'kpc'), north_vector=L)
-#p = yt.OffAxisProjectionPlot(ds, edge2, ('gas', 'density'), center=c, width=(50, 'kpc'), north_vector=L)
+#p = yt.OffAxisProjectionPlot(ds, edge2, ('gas', 'density'), center=c, width=(10, 'kpc'), north_vector=L)
 #p.save()
 
 ##create a phase plot
-#sp2 = ds.sphere(c, (250.0, 'kpc'))
-sp2 = ds.sphere(c, (150.0, 'kpc'))
+sp2 = ds.sphere(c, (200.0, 'kpc'))
 #p = yt.PhasePlot(sp2, ('gas', 'density'), ('gas', 'temperature'), ('gas', 'mass'), weight_field=None)
 #p.set_unit(('gas', 'mass'), 'Msun')
 #p.save()
@@ -114,62 +112,62 @@ sp2.set_field_parameter('normal', L)
 #p.save()
 
 ##apply angle filters
-#p1 = yt.PhasePlot(sp2, ('angle_low', 'particle_position_spherical_radius'), \
-#    ('angle_low', 'phi'), ('angle_low', 'mass'), weight_field=None)
-#p1.set_unit(('angle_low', 'mass'), 'Msun')
-#p1.set_unit(('angle_low', 'particle_position_spherical_radius'), 'kpc')
-#p1.set_log(('angle_low', 'phi'), False)
+#p1 = yt.PhasePlot(sp2, ('angle_I', 'particle_position_spherical_radius'), \
+#    ('angle_I', 'phi'), ('angle_I', 'mass'), weight_field=None)
+#p1.set_unit(('angle_I', 'mass'), 'Msun')
+#p1.set_unit(('angle_I', 'particle_position_spherical_radius'), 'kpc')
+#p1.set_log(('angle_I', 'phi'), False)
 #p1.set_xlim(1e-2, 150)
 #p1.set_ylim(0, 90)
-#p1.set_zlim(('angle_low', 'mass'), 5e4, 1e9)
+#p1.set_zlim(('angle_I', 'mass'), 5e4, 1e9)
 #p1.save()
 
-#p2 = yt.PhasePlot(sp2, ('angle_low_mid', 'particle_position_spherical_radius'), \
-#    ('angle_low_mid', 'phi'), ('angle_low_mid', 'mass'), weight_field=None)
-#p2.set_unit(('angle_low_mid', 'mass'), 'Msun')
-#p2.set_unit(('angle_low_mid', 'particle_position_spherical_radius'), 'kpc')
-#p2.set_log(('angle_low_mid', 'phi'), False)
+#p2 = yt.PhasePlot(sp2, ('angle_II', 'particle_position_spherical_radius'), \
+#    ('angle_II', 'phi'), ('angle_II', 'mass'), weight_field=None)
+#p2.set_unit(('angle_II', 'mass'), 'Msun')
+#p2.set_unit(('angle_II', 'particle_position_spherical_radius'), 'kpc')
+#p2.set_log(('angle_II', 'phi'), False)
 #p2.set_xlim(1e-2, 150)
 #p2.set_ylim(0, 90)
-#p2.set_zlim(('angle_low_mid', 'mass'), 5e4, 1e9)
+#p2.set_zlim(('angle_II', 'mass'), 5e4, 1e9)
 #p2.save()
 
-#p3 = yt.PhasePlot(sp2, ('angle_mid_one', 'particle_position_spherical_radius'), \
-#    ('angle_mid_one', 'phi'), ('angle_mid_one', 'mass'), weight_field=None)
-#p3.set_unit(('angle_mid_one', 'mass'), 'Msun')
-#p3.set_unit(('angle_mid_one', 'particle_position_spherical_radius'), 'kpc')
-#p3.set_log(('angle_mid_one', 'phi'), False)
+#p3 = yt.PhasePlot(sp2, ('angle_III', 'particle_position_spherical_radius'), \
+#    ('angle_III', 'phi'), ('angle_III', 'mass'), weight_field=None)
+#p3.set_unit(('angle_III', 'mass'), 'Msun')
+#p3.set_unit(('angle_III', 'particle_position_spherical_radius'), 'kpc')
+#p3.set_log(('angle_III', 'phi'), False)
 #p3.set_xlim(1e-2, 150)
 #p3.set_ylim(0, 90)
-#p3.set_zlim(('angle_mid_one', 'mass'), 5e4, 1e9)
+#p3.set_zlim(('angle_III', 'mass'), 5e4, 1e9)
 #p3.save()
 
-#p4 = yt.PhasePlot(sp2, ('angle_mid_two', 'particle_position_spherical_radius'), \
-#    ('angle_mid_two', 'phi'), ('angle_mid_two', 'mass'), weight_field=None)
-#p4.set_unit(('angle_mid_two', 'mass'), 'Msun')
-#p4.set_unit(('angle_mid_two', 'particle_position_spherical_radius'), 'kpc')
-#p4.set_log(('angle_mid_two', 'phi'), False)
+#p4 = yt.PhasePlot(sp2, ('angle_IV', 'particle_position_spherical_radius'), \
+#    ('angle_IV', 'phi'), ('angle_IV', 'mass'), weight_field=None)
+#p4.set_unit(('angle_IV', 'mass'), 'Msun')
+#p4.set_unit(('angle_IV', 'particle_position_spherical_radius'), 'kpc')
+#p4.set_log(('angle_IV', 'phi'), False)
 #p4.set_xlim(1e-2, 150)
 #p4.set_ylim(0, 90)
-#p4.set_zlim(('angle_mid_two', 'mass'), 5e4, 1e9)
+#p4.set_zlim(('angle_IV', 'mass'), 5e4, 1e9)
 #p4.save()
 
-#p5 = yt.PhasePlot(sp2, ('angle_mid_high', 'particle_position_spherical_radius'), \
-#    ('angle_mid_high', 'phi'), ('angle_mid_high', 'mass'), weight_field=None)
-#p5.set_unit(('angle_mid_high', 'mass'), 'Msun')
-#p5.set_unit(('angle_mid_high', 'particle_position_spherical_radius'), 'kpc')
-#p5.set_log(('angle_mid_high', 'phi'), False)
+#p5 = yt.PhasePlot(sp2, ('angle_V', 'particle_position_spherical_radius'), \
+#    ('angle_V', 'phi'), ('angle_V', 'mass'), weight_field=None)
+#p5.set_unit(('angle_V', 'mass'), 'Msun')
+#p5.set_unit(('angle_V', 'particle_position_spherical_radius'), 'kpc')
+#p5.set_log(('angle_V', 'phi'), False)
 #p5.set_xlim(1e-2, 150)
 #p5.set_ylim(0, 90)
-#p5.set_zlim(('angle_mid_high', 'mass'), 5e4, 1e9)
+#p5.set_zlim(('angle_V', 'mass'), 5e4, 1e9)
 #p5.save()
 
-#p6 = yt.PhasePlot(sp2, ('angle_high', 'particle_position_spherical_radius'), \
-#    ('angle_high', 'phi'), ('angle_high', 'mass'), weight_field=None)
-#p6.set_unit(('angle_high', 'mass'), 'Msun')
-#p6.set_unit(('angle_high', 'particle_position_spherical_radius'), 'kpc')
-#p6.set_log(('angle_high', 'phi'), False)
+#p6 = yt.PhasePlot(sp2, ('angle_VI', 'particle_position_spherical_radius'), \
+#    ('angle_VI', 'phi'), ('angle_VI', 'mass'), weight_field=None)
+#p6.set_unit(('angle_VI', 'mass'), 'Msun')
+#p6.set_unit(('angle_VI', 'particle_position_spherical_radius'), 'kpc')
+#p6.set_log(('angle_VI', 'phi'), False)
 #p6.set_xlim(1e-2, 150)
 #p6.set_ylim(0, 90)
-#p6.set_zlim(('angle_high', 'mass'), 5e4, 1e9)
+#p6.set_zlim(('angle_VI', 'mass'), 5e4, 1e9)
 #p6.save()
